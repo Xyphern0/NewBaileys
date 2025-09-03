89cb68f418e8d500f431f6575fcad7c66a53959d
Xyphern0 <ardanimuktifutsall@gmail.com>
2025-09-03 18:27:25 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import makeWASocket from './Socket/index'
+  GNU nano 8.6           index.ts
+// --- Banner Log ---
+// \x1b[33m = kuning, \x1b[0m = reset warna
+console.log("\x1b[33m%s\x1b[0m", "Thanks you for usin>
 
+import pino from 'pino'
+import makeWASocketOriginal from './Socket'
+
+// re-export biar tetap kompatibel dengan repo asli
 export * from '../WAProto/index.js'
 export * from './Utils/index'
 export * from './Types/index'
@@ -8,6 +15,18 @@ export * from './WABinary/index'
 export * from './WAM/index'
 export * from './WAUSync/index'
 
+// bikin logger custom
+const logger = pino({ level: 'info' })
+
+// wrapper fungsi utama supaya tiap kali dipanggil ad>
+function makeWASocket(...args: Parameters<typeof make>
+    logger.info("📲 Someone started a Baileys session>
+    return makeWASocketOriginal(...args)
+}
+
+// tipe bawaan dari socket
 export type WASocket = ReturnType<typeof makeWASocket>
+
+// export ulang fungsi
 export { makeWASocket }
 export default makeWASocket
